4dfe5a07d549da0f96ae73f4144b75ddd1584025
hawkinswinja <hawkinswinja@gmail.com>
2023-03-05 18:10:37 +0300

    updated handling exceptions for voting and admin pages

--- app/test.js
+++
@@ -1,6 +0,0 @@
-$('button[name="search"]').on('click', function() {
-	fetch('localhost:5000/posts')
-	.then(response => response.json())
-	.then(data => alert(data))
-	.catch(error => alert(error));
-});

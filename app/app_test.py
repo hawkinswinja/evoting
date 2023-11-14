34cf9a392eeb3ca7d66027bcd34989f18af7eb76
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-11-14 22:50:24 +0300

    Update app_test.py

--- app/app_test.py
+++ app/app_test.py
@@ -2,12 +2,14 @@ from models import storage, auth
 
 storage.new('Voter', {'name': 'admin', 'email': 'admin@ikura',
                       'auth_id': auth.get_hash('admin23').decode()})
-for i in range(2, 26):
+for i in range(5):
     name = 'student' + str(i)
     email = name + '@email.com'
     pw = auth.get_hash('pw' + name).decode()
     storage.new('Voter', {'name': name, 'email': email, 'auth_id': pw})
 storage.save()
 users = storage.all('Voter')
-for user in users:
-    print(user.id, user.name, user.status)
+if len(users) == 5:
+  print('Tests Passed')
+else:
+  print('Failed tests')

c5cd8cc65568bcee74054351b21ef521cac5ab20
hawkinswinja <hawkinswinja@gmail.com>
2024-07-23 16:46:47 +0300

    update: improve the ui of the portal page description: add a new css file to the portal page to improve the ui

--- ikura-app/app/static/scripts/portal.js
+++ ikura-app/app/static/scripts/portal.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
 
-  $('button[name="results"]').on('click', function() {
-      const url = '/candidates/' + $('#filterpost').val();
-	  fetch(url)
-	  .then(response => response.json())
-	  .then(data => {
-		$('#candidates li').empty();
-		const list = document.getElementById('candidates');
-		// list.html("<h3>Name&emsp;Position&ensp;Votes</h3>")
-		for (const cand of Object.values(data)) {
-			const li = document.createElement('li');
-			li.innerHTML = cand.name + '&emsp;' + cand.post + '&emsp;' + cand.votes;
-			list.appendChild(li);
-		}
-	  })	  
-	  .catch(error => alert(error));
+	$('button[name="results"]').on('click', function() {
+		const url = '/candidates/' + $('#filterpost').val();
+		fetch(url)
+		.then(response => response.json())
+		.then(data => {
+		  $('#candidates').empty(); // Clear existing list items
+		  const list = document.getElementById('candidates');
+		  const header = document.createElement('h3');
+		  header.innerHTML = "Name&emsp;Position&ensp;Votes";
+		  list.appendChild(header);
+		  for (const cand of Object.values(data)) {
+			  const li = document.createElement('li');
+			  li.innerHTML = cand.name + '&emsp;' + cand.post + '&emsp;' + cand.votes;
+			  list.appendChild(li);
+		  }
+		})	  
+		.catch(error => alert(error));
+	});
+  
+	$('.exit').on('click', () => { window.location = "/logout"; });
+  
   });
-
-
-  $('.exit').on('click', () => { window.location = "/logout"; });
-
-});
+  
